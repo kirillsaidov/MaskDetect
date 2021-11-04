6402589bfcd2f4dfbd11377438ac8c998049de49
kirill.saidov <kirill.saidov.d@gmail.com>
2021-11-04 16:19:48 +0600

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <opencv2/dnn/dnn.hpp>
+
+constexpr int esc_key = 27;
 
 int main() {
-    std::cout << "hello, world!\n";
+    //cv::dnn::Net net = cv::dnn::readNetFromONNX("__internal/weights/weights.onnx");
+    auto net = cv::dnn::readNet("__internal/weights/last.onnx");
+    cv::VideoCapture cap;
+    if(!cap.open(0)) {
+        std::cout << "Failed to open camera!\n";
+        return 0;
+    }
+
+    while(true) {
+        cv::Mat frame;
+        cap.read(frame);
+
+        cv::imshow("vid", frame);
+
+        if(cv::waitKey(30) == esc_key) {
+            break;
+        }
+    }
+
+    cv::destroyAllWindows();
+    cap.release();
+
     return 0;
 }
